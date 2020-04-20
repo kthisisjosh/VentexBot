8059e3a1723aa8aad1640afa0276d5fe6ffb9399
Joshua <jbautista396@gmail.com>
2020-04-20 11:25:20 -0400

    Add 'sendscreenshot' command.

--- src/bot.js
+++ src/bot.js
@@ -14,7 +14,7 @@ const bot = new Eris.CommandClient(
     {
         description: "Ventex Bot",
         owner: "Simplistics",
-        prefix: "~",
+        prefix: ["~", "!"],
     }
 );
 
@@ -27,6 +27,7 @@ const bot = new Eris.CommandClient(
 bot.registerCommand("rsm", "React (can react multiple times) to get a random screenshot memory from #screenshot-memories.", {
     description: "Gets a random screenshot memory from the #screenshot-memories channel. Put some in there!",
     fullDescription: "Gets a random screenshot memory from the #screenshot-memories channel. Put some in there!",
+    caseInsensitive: true,
     reactionButtons: [
         {
             emoji: "♻️",
@@ -38,6 +39,26 @@ bot.registerCommand("rsm", "React (can react multiple times) to get a random scr
     ],
     reactionButtonTimeout: 100000000,
 });
+
+bot.registerCommand(
+    "sendscreenshot",
+    (msg, args) => {
+        if (args.length === 0) {
+            return "Invalid input, send a link to a screenshot: '~sendscreenshot <link-to-screenshot>'";
+        } else if (args.length > 1) {
+            return "Invalid input, send a link ONLY.";
+        } else {
+            console.log(args[0]);
+            return "Sent.";
+        }
+    },
+    {
+        description: "Send a screenshot to be added.",
+        fullDescription: "Sends a screenshot to be added.",
+        caseInsensitive: true,
+        usage: "<text>",
+    }
+);
 
 /**
  * Down to play related commands.
@@ -68,6 +89,8 @@ bot.registerCommand("Valorant10Man", "@here React ONCE if you want to play a Val
     reactionButtonTimeout: 500000,
 });
 
+bot.registerCommandAlias("val10man", "Valorant10Man");
+
 bot.registerCommand("Valorant5Man", "@here React ONCE if you're down to play a Valorant 5 man. \n\n Down to play: \n\n", {
     description: "Will ask mans if they want to play a Valorant 5 man with a @ here.",
     fullDescription: "Val 5 man",
@@ -90,6 +113,8 @@ bot.registerCommand("Valorant5Man", "@here React ONCE if you're down to play a V
     ],
     reactionButtonTimeout: 500000,
 });
+
+bot.registerCommandAlias("val5man", "Valorant5Man");
 
 bot.registerCommand("tft", "@here React ONCE if you're down to play TFT. \n\n Down to play: \n\n", {
     description: "Will ask mans if they want to play tft with a @ here.",
@@ -123,21 +148,21 @@ bot.registerCommand(
     "send",
     (msg, args) => {
         if (args.length === 0) {
-            return "Invalid input, send something: '~send <something>'";
+            return "Invalid input, send something: '~send <text>'";
         }
         const toSend = args.join(" ");
         console.log(toSend);
     },
     {
-        description: "Send something to josh (test command)",
-        fullDescription: "Sends something to josh (test command)",
+        description: "Send something to josh (test command).",
+        fullDescription: "Sends something to josh (test command).",
         usage: "<text>",
     }
 );
 
 bot.registerCommand("code", "https://github.com/kthisisjosh/VentexBot", {
-    description: "View the code of the bot",
-    fullDescription: "View the code of the bot",
+    description: "View the code of the bot.",
+    fullDescription: "View the code of the bot.",
 });
 
 //-----------------------------------------------------------------------------------------------------------------------\\

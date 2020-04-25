435eedbba1f2633d1023d79008aca2cde5f4d08b
Joshua <jbautista396@gmail.com>
2020-04-24 23:34:28 -0400

    Add screenshots

--- src/screenshots.js
+++ src/screenshots.js
@@ -84,6 +84,7 @@ const screenshots = {
         "https://cdn.discordapp.com/attachments/340703597979500545/701850769527406612/image0.png",
         "https://cdn.discordapp.com/attachments/340703597979500545/701850862171193434/image0.png",
         "https://cdn.discordapp.com/attachments/340703597979500545/701850251593646101/image0.png",
+        "https://cdn.discordapp.com/attachments/340703597979500545/701907836153626634/unknown.png",
     ],
 };
 
